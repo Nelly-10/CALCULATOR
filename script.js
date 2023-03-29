980e0ad7902ed06061cc8cf0b83684586c796156
NELLY-10 <nelson__nonso@yahoo.com>
2023-03-29 18:07:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+'use script';
+
+
+
+// (function() {
+//     let screen = document.querySelector('.screen');
+//      let btns = document.querySelectorAll('.btn');
+//      let equal = document.querySelector('.btn-equal');
+//      let clear = document.querySelector('.btn-clear');
+
+//      btns.forEach(function(button) {
+//         button.addEventListener('click', function(e) {
+//             let value = e.target.dataset.num;
+//             screen.value += value;
+//         })
+//      })
+
+//     equal.addEventListener('click', function() {
+//         if (screen.value === '') {
+//             screen.value = "";
+//         } else {
+//             let answer = eval(screen.value);
+//             screen.value = answer;
+//         }
+//     })
+// })()
